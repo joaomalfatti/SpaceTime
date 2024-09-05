03d82fcb3fc9e0e7de0620399c46ebb06875b366
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-04 21:01:06 -0300

    Foi feito a parte de oauth e logo depois criamos a parte de components e fizemos a configurações necessárias para que a page puxa a configuração feita

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { User } from 'lucide-react';
-import Image from 'next/image';
-
-import nlwLogo from '../assets/nlw-spacetime-logo.svg';
+import { Copyright } from '@/components/Copyright';
+import { Hero } from '@/components/Hero';
+import { SignIn } from '@/components/SignIn';
+import { EmptyMemories } from '@/components/EmptyMemories';
 
 export default function Home() {
   return (
@@ -16,53 +17,17 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"/>
 
         {/* Sign in+ */}
-        <a href="" className=" flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="max-w-[140px]  text-sm leading-snug">
-            <span className="underline"> Crie sua conta</span> e salve suas memórias!
-          </p>
-        </a>
-
+        <SignIn />
         {/* Hero */}
-        <div className="space-y-5">
-
-          <Image src={nlwLogo} alt="NLW SpaceTime" />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
-            <p className="text-lg leading-relaxed"> Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
-          </div>
-
-          <a href="" className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600">
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
-
+        <Hero />
         {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💚 no NLW da {""}
-          <a 
-            href="https://www.instagram.com/joao_elias1210/?next=%2F"
-            rel="noreferrer"
-            className='underline hover:text-gray-100'
-            target="_blank"
-          >
-            João Malfatti
-          </a>
-        </div>
+        <Copyright />
 
       </div>
+
       {/*Right*/}
       <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="text-center leading-relaxed w-[360px]">
-            Você ainda não registrou nenhuma lembrança, comece a {" "} 
-            <a href="" className="underline hover:text-gray-50"> criar agora!</a>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   );
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return(
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💚 no NLW da {""}
+      <a 
+        href="https://www.instagram.com/joao_elias1210/?next=%2F"
+        rel="noreferrer"
+        className='underline hover:text-gray-100'
+        target="_blank"
+      >
+        João Malfatti
+    </a>
+  </div>
+  )
+}
